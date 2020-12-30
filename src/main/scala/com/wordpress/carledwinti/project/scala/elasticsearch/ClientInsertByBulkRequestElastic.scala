596b975c357fbae2cebe3193df6a6b288b04f752
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2020-12-30 14:24:35 -0300

    Implementação do BulkRequest

--- src/main/scala/com/wordpress/carledwinti/project/scala/elasticsearch/ClientInsertByBulkRequestElastic.scala
+++ src/main/scala/com/wordpress/carledwinti/project/scala/elasticsearch/ClientInsertByBulkRequestElastic.scala
@@ -4,7 +4,7 @@ import org.apache.http.HttpHost
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.elasticsearch.action.admin.indices.alias.Alias
 import org.elasticsearch.action.bulk.{BulkRequest, BulkResponse}
-import org.elasticsearch.action.index.{IndexRequest, IndexResponse}
+import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.indices.{CreateIndexRequest, CreateIndexResponse}
 import org.elasticsearch.client.{RequestOptions, RestClient, RestClientBuilder, RestHighLevelClient}
 import org.elasticsearch.common.settings.Settings
@@ -12,18 +12,55 @@ import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory, XCon
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import scala.collection.JavaConversions.asJavaCollection
 
-object ClientReadParquetCpfHadoopSaveBulkElastic {
+object ClientInsertByBulkRequestElastic {
+
   def main(args: Array[String]): Unit = {
+
     System.setProperty("hadoop.home.dir", "C:\\hadoop")
+
     val spark = SparkSession.builder.appName("ClientWriteReadParquet").config("spark.master", "local").getOrCreate()
     val pathParquetFile: String = "hdfs://cloudera@192.168.56.102/user/cloudera"
     val cpfsDF = parquetReader(spark, pathParquetFile).toDF("cpf")
+    val restClientBuilder: RestClientBuilder = RestClient.builder(new HttpHost("localhost", 9200, "http"))
+    val restHighLevelClient: RestHighLevelClient = new RestHighLevelClient(restClientBuilder)
+    val createIndextResponse: CreateIndexResponse = executeCreateIndex(restHighLevelClient, createIndexRequest("cpf", createPropertiesWithXContentBuilder))
+    val cpfList: List[Any] = convertCpfDFToList(cpfsDF)
+    val bulkRequest: BulkRequest = new BulkRequest()
+
+    addAllCpfsOnBullRequest(createIndextResponse.index, cpfList, bulkRequest)
+
+    val bulkResponse: BulkResponse = restHighLevelClient.bulk(bulkRequest,RequestOptions.DEFAULT)
+    println("bulk status " + bulkResponse.status)
+
+    restHighLevelClient.close
+    spark.close
+  }
+
+  def addAllCpfsOnBullRequest(indexName: String, cpfList: List[Any], bulkRequest: BulkRequest): Unit ={
+    cpfList.toStream.foreach(c => {
+      println("------------------->  :" + c)
+      bulkRequest.add(new IndexRequest(indexName).source(XContentType.JSON, "cpf", c.toString))
+    })
+  }
+
+  def convertCpfDFToList(cpfsDF: DataFrame): List[Any] = {
+    cpfsDF.select("cpf").rdd.map(r =>  r(0)).collect().toList
+  }
+
+  def executeCreateIndex(restHighLevelClient: RestHighLevelClient, createIndexRequest: CreateIndexRequest): CreateIndexResponse = {
+    val createIndextResponse: CreateIndexResponse = restHighLevelClient.indices.create(createIndexRequest, RequestOptions.DEFAULT)
+    println("result " + createIndextResponse.isAcknowledged)
+    println("index " + createIndextResponse.index)
+    createIndextResponse
+  }
+
+  def createIndexRequest(name: String, xContentBuilder: XContentBuilder): CreateIndexRequest = {
 
     val dtf: DateTimeFormatter = DateTimeFormatter.ofPattern("ddMMyyyyHHmmss")
     val now = LocalDateTime.now()
-    val indexName = "cpfs" + now.format(dtf)
+    val indexName = name + now.format(dtf)
+
     val createIndexRequest: CreateIndexRequest = new CreateIndexRequest(indexName)
     createIndexRequest.settings(Settings
       .builder
@@ -32,22 +69,9 @@ object ClientReadParquetCpfHadoopSaveBulkElastic {
 
     val alias: Alias = new Alias("cpfs_alias")
     createIndexRequest.alias(alias)
-    createIndexRequest.mapping(createPropertiesWithXContentBuilder)
+    createIndexRequest.mapping(xContentBuilder)
 
-    val restClientBuilder: RestClientBuilder = RestClient.builder(new HttpHost("localhost", 9200, "http"))
-    val restHighLevelClient: RestHighLevelClient = new RestHighLevelClient(restClientBuilder)
-
-    val createIndextResponse: CreateIndexResponse = restHighLevelClient.indices.create(createIndexRequest, RequestOptions.DEFAULT)
-    println("result " + createIndextResponse.isAcknowledged)
-    println("index " + createIndextResponse.index)
-
-    val bulkRequest: BulkRequest = new BulkRequest()
-    bulkRequest.add(new IndexRequest(createIndextResponse.index).source(XContentType.JSON, "cpf", "5645454"))
-
-    val bulkResponse: BulkResponse = restHighLevelClient.bulk(bulkRequest,RequestOptions.DEFAULT)
-    println("bulk status " + bulkResponse.status)
-    restHighLevelClient.close
-    spark.close
+    createIndexRequest
   }
 
   def createPropertiesWithXContentBuilder: XContentBuilder = {
@@ -55,17 +79,6 @@ object ClientReadParquetCpfHadoopSaveBulkElastic {
     xContentBuilder.startObject
     xContentBuilder.startObject("properties")
     xContentBuilder.startObject("cpf").field("type", "text")
-    xContentBuilder.endObject
-    xContentBuilder.endObject
-    xContentBuilder.endObject
-    xContentBuilder
-  }
-
-  def createCpfWithXContentBuilder: XContentBuilder = {
-    val xContentBuilder: XContentBuilder = XContentFactory.jsonBuilder
-    xContentBuilder.startObject
-    xContentBuilder.startObject("index").field("")
-    xContentBuilder.startObject("cpf").field("33333333333333")
     xContentBuilder.endObject
     xContentBuilder.endObject
     xContentBuilder.endObject
